5f5165980de967ca5f2bd413413196ad1375a38a
Josue santos <josue.santos.dev@gmail.com>
2024-07-04 13:39:19 -0300

    get account endpoint

--- src/ports/service.py
+++ src/ports/service.py
@@ -8,19 +8,8 @@ app = FastAPI()
 def home():
     return {"say": "Hello, world"}
 
-@app.get("/deliveries")
-def deliveries():
-    return [{"id": 1,
-             "status": "done",
-             "price": 10000},
-            {"id": 2,
-             "status": "done",
-             "price": 14300},
-            {"id": 3,
-             "status": "done",
-             "price": 5000}] 
-
-@app.post("/deliveries/quote")
-def delivery_quote(quote: Quote):
-    return {"quote": ((quote.size1 + quote.size2 + quote.size3) / 3 + quote.weight) / 5,
-            "time": 15}
+@app.get("/account")
+def account():
+    return {"account":
+            {"name": "josue Santos",
+             "branch": "009273-1"}}
